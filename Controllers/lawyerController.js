42da9b592025982c6a24a9090b084454800a2dc5
Dr-AddictStein <dr.addictstein@gmail.com>
2024-07-26 02:52:17 +0600

    reset-password-done

--- Controllers/lawyerController.js
+++ Controllers/lawyerController.js
@@ -2,6 +2,7 @@ import lawyerModel from '../models/LawyerModel.js';
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
 import nodemailer from "nodemailer";
+import bcrypt from 'bcrypt'
 
 const createToken = (_id) => {
     return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' });
@@ -38,6 +39,49 @@ export const sendOTP = async (req, res) => {
         // console.log(date.email)
         subject: `Email verification for Lawyer Registration `,
         text: `Hi ${data.name},\nThank you for choosing LawEase\nHere is your OTP for Email Verification ${otp}\nLooking forward to help you.\nTeam LawEase`,
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Email sent: " + info.response);
+        }
+    });
+
+    const toSend=otp.toString();
+
+    return res.status(201).json(toSend);
+
+}
+export const sendOTPForForgetPassword = async (req, res) => {
+    const data = {
+        email: req.body.email,
+    };
+    const sa = Date.now().toString();
+
+    const mid  = parseInt(sa);
+
+    let otp = mid%1000000;
+
+    while (otp<1000000) {
+        otp = otp*10;
+    }
+
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "lawease24@gmail.com",
+            pass: "caou envd jhnj tmxl",
+        },
+    });
+
+    var mailOptions = {
+        from: "lawease24@gmail.com",
+        to: data.email,
+        // console.log(date.email)
+        subject: `Email verification for Forget Password `,
+        text: `Hi,\nHere is your OTP for Email Verification and further process for the Forget Password: ${otp}\nLooking forward to help you.\nTeam LawEase`,
     };
 
     transporter.sendMail(mailOptions, function (error, info) {
@@ -115,6 +159,27 @@ export const updateLawyer = async (req, res) => {
 
     if (lawyer) {
         res.status(200).json(lawyer);
+    } else {
+        return res.status(400).json({ error: "No Such Lawyer Found.!." });
+    }
+};
+
+
+export const updatePassword = async (req, res) => {
+    const { email } = req.params;
+
+
+    const password=req.body.password;
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const lawyer = await lawyerModel.findOneAndUpdate({ email: email }, {
+        password:hash
+    });
+
+    if (lawyer) {
+        return res.status(200).json(lawyer);
     } else {
         return res.status(400).json({ error: "No Such Lawyer Found.!." });
     }
--- Routes/lawyerRoutes.js
+++ Routes/lawyerRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllLawyer, getSingleLawyer, loginUser, postReview, sendOTP, singupUser, updateLawyer } from '../Controllers/lawyerController.js';
+import { getAllLawyer, getSingleLawyer, loginUser, postReview, sendOTP, sendOTPForForgetPassword, singupUser, updateLawyer, updatePassword } from '../Controllers/lawyerController.js';
 
 
 
@@ -9,11 +9,13 @@ const router = express.Router();
 
 
 router.post('/login',loginUser)
+router.post('/login/forgetpassword',sendOTPForForgetPassword)
 router.post('/signup',singupUser)
 router.post('/signup/sendotp',sendOTP)
 router.get('/getLawyer/:id',getSingleLawyer)
 router.get('/getLawyer',getAllLawyer)
 router.patch('/updateLawyer/:id',updateLawyer)
+router.patch('/updatePassword/:email',updatePassword)
 router.patch('/postreview/:id',postReview)
 
 
